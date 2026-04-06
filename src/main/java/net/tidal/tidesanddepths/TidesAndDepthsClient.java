10a7368e9b5ed32a34da045e2065b4da7ccc88ba
ThanisTheFox <jonah.grass2008@gmail.com>
2026-04-06 22:58:31 +0200

    + Client

---
+++ src/main/java/net/tidal/tidesanddepths/TidesAndDepthsClient.java
@@ -0,0 +1,11 @@
+package net.tidal.tidesanddepths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TidesAndDepthsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Exploring the deep oceans...");
+
+    }
+}
