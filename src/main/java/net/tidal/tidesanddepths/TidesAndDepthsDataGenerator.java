83b41f837feff69822ab63e339844c54ea4a7c6a
ThanisTheFox <jonah.grass2008@gmail.com>
2026-04-06 22:52:54 +0200

    Initial commit

---
+++ src/main/java/net/tidal/tidesanddepths/TidesAndDepthsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tidal.tidesanddepths;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TidesAndDepthsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
